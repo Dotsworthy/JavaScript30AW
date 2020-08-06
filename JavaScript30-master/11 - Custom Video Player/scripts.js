1bcdbd8160c79655fbba619e87681ff352ffdfb6
Dotsworthy <dotsworthy@gmail.com>
2020-08-06 15:42:37 +0100

    added fullscreen playback

--- JavaScript30-master/11 - Custom Video Player/scripts.js
+++ JavaScript30-master/11 - Custom Video Player/scripts.js
@@ -5,6 +5,7 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const fullscreen = player.querySelector('.fullscreen');
 
 function togglePlay() {
     const method = video.paused ? 'play' : 'pause';
@@ -34,10 +35,27 @@ function scrub(e) {
     video.currentTime = scrubTime;
 }
 
+
+function toggleFullscreen() {
+    if (video.requestFullscreen) {
+        video.requestFullscreen()
+	} else if(video.webkitRequestFullScreen){
+		video.webkitRequestFullScreen();
+	} else if(video.mozRequestFullScreen){
+		video.mozRequestFullScreen();
+	}
+    console.log(video.requestFullscreen)
+    console.log(video.webkitRequestFullScreen)
+    console.log(video.mozRequestFullScreen)
+}
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
+
+
+fullscreen.addEventListener('click', toggleFullscreen);
 
 
 toggle.addEventListener('click', togglePlay);
